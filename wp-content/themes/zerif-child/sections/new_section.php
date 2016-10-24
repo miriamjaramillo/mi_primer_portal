2dcafe0acd59771f393048522c953bc3325fe010
MAR <MAR>
2016-10-23 22:42:04 -0500

    CAMBIOS QUIENES SOMOS

--- wp-content/themes/zerif-child/sections/new_section.php
+++ wp-content/themes/zerif-child/sections/new_section.php
@@ -1,5 +1,5 @@
 
-<section class="our-team" id="nuevaseccion">
+<section class="our-team" id="quienes_somos">
 <div class="container">
     <!-- SECTION HEADER -->
     <div class="section-header">
@@ -26,7 +26,7 @@
                 dynamic_sidebar( 'new-section-sidebar' );
             else:
 		//the_widget( 'zerif_ourfocus','text=Create memorable pages with smooth parallax effects that everyone loves. Also, use our lightweight content slider offering you smooth and great-looking animations' );
-                $zerif_newsection_text = get_theme_mod('zerif_newsection_text', 'La Cooperativa de Ahorro y Crédito "El Comerciante" Ltda., es una organización jurídica que se encuentra legalmente constituida en el país, en la provincia de Loja, cantón Saraguro; realiza actividades de intermediación financiera, con sujeción a las regulaciones y a los principios reconocidos en la Ley Orgánica de la Economía Popular y Solidaria y del Sector Financiero Popular y Solidario, a su Reglamento General, a las Resoluciones de la Superintendencia de Economía Popular y Solidaria y del ente regulador. <br/><br/><br/> Mgs. Lauro Rodrigo Ordoñez Pachar. <br/><b>GERENTE GENERAL.</b>');
+                $zerif_newsection_text = get_theme_mod('zerif_newsection_text', 'La Cooperativa de Ahorro y Crédito "El Comerciante" Ltda., es una organización jurídica que se encuentra legalmente constituida en el país, en la provincia de Loja, cantón Saraguro; realiza actividades de intermediación financiera, con sujeción a las regulaciones y a los principios reconocidos en la Ley Orgánica de la Economía Popular y Solidaria y del Sector Financiero Popular y Solidario, a su Reglamento General, a las Resoluciones de la Superintendencia de Economía Popular y Solidaria y del ente regulador.');
                 $zerif_newsection_firm = get_theme_mod('zerif_newsection_firm', '');
                 if (!empty($zerif_newsection_text)):
                     echo '<div class="col-lg-' . $colCount . ' col-md-' . $colCount . ' column zerif_about_us_center ' . $text_and_skills . '" data-scrollreveal="enter bottom after 0s over 1s">';
